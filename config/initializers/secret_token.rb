59736713998babac56bb1db3002dc938ca91b6ed
My Name <NARANG@NARANG-VAIO.(none)>
2013-01-08 11:05:28 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrainingApp::Application.config.secret_token = '228daaf86cfaf0e8b789580c8fe8f59ceefd7a1042dde3e3ef6bddc840d9a0deb8b07312c2c8b846fd59211bb955bcfdf92a56f424e7471efa1693bdc971ef0a'
